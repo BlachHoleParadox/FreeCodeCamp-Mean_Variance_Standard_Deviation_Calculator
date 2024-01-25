69977a2f8977e573c0eade473446223b95d2177c
BlackHoleParadox <157750131+BlachHoleParadox@users.noreply.github.com>
2024-01-25 22:26:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Start by reading the README.md file. This file is intended to be used as an entry point during development.
+import mean_var_std
+from unittest import main
+
+print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+
+# Unit Test
+main(module='test_module', exit=False)
